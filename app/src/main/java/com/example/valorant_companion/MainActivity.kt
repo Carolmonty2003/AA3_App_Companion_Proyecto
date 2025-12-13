082baf65c3a9d04563942fc9735196111357d763
Carolmonty <carolina.monteagudo@enti.cat>
2025-12-13 18:44:53 +0100

    top bar design finished + wip top bar design
    -images imported as vector assets and nav bar structure finished
    -start kt code struct to change to another fragments
    -creating top bar code

--- app/src/main/java/com/example/valorant_companion/MainActivity.kt
+++ app/src/main/java/com/example/valorant_companion/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.valorant_companion
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        // Fragment por defecto
+        if (savedInstanceState == null) {
+            //replaceFragment(MapsFragment())
+            bottomNav.selectedItemId = R.id.nav_maps
         }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                //R.id.nav_maps -> replaceFragment(MapsFragment())
+                //R.id.nav_agents -> replaceFragment(AgentsFragment())
+                //R.id.nav_events -> replaceFragment(EventsFragment())
+                //R.id.nav_chat -> replaceFragment(ChatFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
